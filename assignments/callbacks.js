66f2d1ef685d8cf333babf969f2a5630bf28cd8a
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-03-31 21:04:56 -0700

    complete callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -8,49 +8,130 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+
+
+
 
   //Given this problem: 
-  
+
+  // firstItem passes the first item of the given array to the callback function.
   function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
   }
 
   // Potential Solution:
 
   // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
-    return cb(arr[0]);
+    return cb(arr[0])
   }
 
-  // Function invocation 
+  // Function invocation as a normal function
   firstItem(items, function(first) {
     console.log(first)
   });
 
-*/
+  // As an arrow function
+  firstItem(items, (first) => {console.log(first)})
 
 
+
+
+
+
+
+// getLength passes the length of the array into the callback.
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
 
+// As an arrow function
+getLength(items, (length) => {console.log(items.length)})
+
+// As a regular function
+getLength(items, function(length) {
+  console.log(items.length)
+})
+
+
+
+
+
+
+
+
+
+// last passes the last item of the array into the callback.
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr.length-1)
 }
 
+// As an arrow function
+last(items, (last) => {console.log(items[items.length - 1])})
+
+
+
+
+
+
+
+
+
+
+// sumNums adds two numbers (x, y) and passes the result to the callback.
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+
+// As an arrow function
+sumNums(1, 2, (sum) => {console.log(1 + 2)})
+
+
+
+
+
+
+
+
+
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y)
 }
+
+
+// As an arrow function
+multiplyNums(2, 2, (multiply) => {console.log(2 * 2)})
+
+
+
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"]
+
+var n = fruits.includes("Apple")
+
+n
+
+
+
+const dlastItem = items/*?*/
+const lastItem = items[items.length-1]/*?*/
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item))
 }
+
+contains("Pencil", items, (checkArray) => {console.log(items.includes("Pencil"))})
+
+
+
 
 /* STRETCH PROBLEM */
 
